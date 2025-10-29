d77fd9c3bc25cf7c414434d6abefd4e5b4a51a36
Shantanu Parab <shantanu@trossenrobotics.com>
2025-10-29 09:17:39 -0500

    block_transfer_update

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -720,7 +720,7 @@ _CONFIGS = [
             ),
         ),
         weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi05_base/params"),
-        num_train_steps=20_000,
+        num_train_steps=80_000,
         batch_size=2,
     ),
     TrainConfig(
